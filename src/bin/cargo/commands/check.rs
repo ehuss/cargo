e32d9d54c8dfbe0c890cb88c327e22d05399313c
Eric Huss <eric@huss.org>
2021-07-16 06:30:14 -0700

    Crater test.

--- src/bin/cargo/commands/check.rs
+++ src/bin/cargo/commands/check.rs
@@ -40,6 +40,10 @@ pub fn cli() -> App {
 }
 
 pub fn exec(config: &mut Config, args: &ArgMatches<'_>) -> CliResult {
+    if std::env::var("CARGO_REAL_CHECK").is_err() {
+        fixit()?;
+        return Ok(());
+    }
     let ws = args.workspace(config)?;
     let test = match args.value_of("profile") {
         Some("test") => true,
@@ -57,5 +61,50 @@ pub fn exec(config: &mut Config, args: &ArgMatches<'_>) -> CliResult {
     let compile_opts = args.compile_options(config, mode, Some(&ws), ProfileChecking::Unchecked)?;
 
     ops::compile(&ws, &compile_opts)?;
+    Ok(())
+}
+
+fn fixit() -> CliResult {
+    use anyhow::Context;
+    use cargo_util::{paths, ProcessBuilder};
+    use std::path::Path;
+
+    eprintln!("Copying to /tmp/fixit");
+    ProcessBuilder::new("cp")
+        .args(&["-a", ".", "/tmp/fixit"])
+        .exec()?;
+    std::env::set_current_dir("/tmp/fixit").map_err(|e| anyhow::format_err!("cd failed {}", e))?;
+
+    let mut manifest = paths::read(Path::new("Cargo.toml"))?;
+    let ed_re = regex::Regex::new(r#"(?m)^ *edition *= *['"]([^'"]+)['"]"#).unwrap();
+    let man_clone = manifest.clone();
+    let ed_cap = match ed_re.captures(&man_clone) {
+        None => {
+            eprintln!("no edition found in manifest, probably 2015, skipping");
+            return Ok(());
+        }
+        Some(caps) => caps.get(1).unwrap(),
+    };
+    if ed_cap.as_str() != "2018" {
+        eprintln!("skipping non-2018 edition `{}`", ed_cap.as_str());
+        return Ok(());
+    }
+    eprintln!("Running `cargo fix --edition`");
+    // Skip "cargo check"
+    let args: Vec<_> = std::env::args().skip(2).collect();
+    ProcessBuilder::new("cargo")
+        .args(&["fix", "--edition", "--allow-no-vcs", "--allow-dirty"])
+        .args(&args)
+        .exec()
+        .with_context(|| "failed to migrate to next edition")?;
+    manifest.replace_range(ed_cap.range(), "2021");
+    paths::write("Cargo.toml", manifest)?;
+    eprintln!("Running `cargo check` to verify 2021");
+    ProcessBuilder::new("cargo")
+        .args(&["check"])
+        .args(&args)
+        .env("CARGO_REAL_CHECK", "1")
+        .exec()
+        .with_context(|| "failed to check after updating to 2021")?;
     Ok(())
 }
