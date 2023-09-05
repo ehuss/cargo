982e183fb6df384c4a68671dfc2ca3c00f77a7dc
Eric Huss <eric@huss.org>
2023-09-05 15:08:26 -0700

    Simplify du

--- crates/cargo-util/src/du.rs
+++ crates/cargo-util/src/du.rs
@@ -2,10 +2,10 @@
 
 use anyhow::{Context, Result};
 use ignore::overrides::OverrideBuilder;
-use ignore::{DirEntry, ParallelVisitor, ParallelVisitorBuilder, WalkBuilder, WalkState};
+use ignore::{WalkBuilder, WalkState};
 use std::path::Path;
 use std::sync::atomic::{AtomicU64, Ordering};
-use std::sync::{Arc, Condvar, Mutex};
+use std::sync::{Arc, Mutex};
 
 /// Determines the disk usage of all files in the given directory.
 ///
@@ -21,71 +21,6 @@ use std::sync::{Arc, Condvar, Mutex};
 /// nice to improve this or replace it with something better.
 pub fn du(path: &Path, patterns: &[&str]) -> Result<u64> {
     du_inner(path, patterns).with_context(|| format!("failed to walk `{}`", path.display()))
-}
-
-/// Builds directory visitors, where there is one visitor per thread.
-///
-/// When this is dropped, then all visiting has been completed.
-struct DuBuilder {
-    /// A condition variable triggered (via the drop impl) with the walking is finished.
-    finish_cv: Arc<(Mutex<bool>, Condvar)>,
-    /// The total number of bytes.
-    total: Arc<AtomicU64>,
-    /// A slot used to indicate there was an error while walking.
-    ///
-    /// It is possible that more than one error happens (such as in different
-    /// threads). The error returned is arbitrary in that case.
-    err: Arc<Mutex<Option<anyhow::Error>>>,
-}
-
-impl<'s> ParallelVisitorBuilder<'s> for DuBuilder {
-    fn build(&mut self) -> Box<dyn ParallelVisitor + 's> {
-        Box::new(DuVisitor {
-            total: self.total.clone(),
-            err: self.err.clone(),
-        })
-    }
-}
-
-/// A visitor, created by [`DuBuilder`], which is responsible for collecting
-/// file sizes.
-///
-/// There is one visitor per thread.
-struct DuVisitor {
-    total: Arc<AtomicU64>,
-    err: Arc<Mutex<Option<anyhow::Error>>>,
-}
-
-impl ParallelVisitor for DuVisitor {
-    fn visit(&mut self, entry: Result<DirEntry, ignore::Error>) -> WalkState {
-        match entry {
-            Ok(entry) => match entry.metadata() {
-                Ok(meta) => {
-                    if meta.is_file() {
-                        self.total.fetch_add(meta.len(), Ordering::SeqCst);
-                    }
-                }
-                Err(e) => {
-                    *self.err.lock().unwrap() = Some(e.into());
-                    return WalkState::Quit;
-                }
-            },
-            Err(e) => {
-                *self.err.lock().unwrap() = Some(e.into());
-                return WalkState::Quit;
-            }
-        }
-        WalkState::Continue
-    }
-}
-
-impl Drop for DuBuilder {
-    fn drop(&mut self) {
-        let (lock, cvar) = &*self.finish_cv;
-        let mut finished = lock.lock().unwrap();
-        *finished = true;
-        cvar.notify_one();
-    }
 }
 
 fn du_inner(path: &Path, patterns: &[&str]) -> Result<u64> {
@@ -106,25 +41,34 @@ fn du_inner(path: &Path, patterns: &[&str]) -> Result<u64> {
         .git_exclude(false);
     let walker = builder.build_parallel();
     let total = Arc::new(AtomicU64::new(0));
-
-    let finish_cv = Arc::new((Mutex::new(false), Condvar::new()));
-
+    // A slot used to indicate there was an error while walking.
+    //
+    // It is possible that more than one error happens (such as in different
+    // threads). The error returned is arbitrary in that case.
     let err = Arc::new(Mutex::new(None));
-    {
-        let mut visit_builder = DuBuilder {
-            finish_cv: finish_cv.clone(),
-            total: total.clone(),
-            err: err.clone(),
-        };
-        walker.visit(&mut visit_builder);
-    }
+    walker.run(|| {
+        Box::new(|entry| {
+            match entry {
+                Ok(entry) => match entry.metadata() {
+                    Ok(meta) => {
+                        if meta.is_file() {
+                            total.fetch_add(meta.len(), Ordering::SeqCst);
+                        }
+                    }
+                    Err(e) => {
+                        *err.lock().unwrap() = Some(e.into());
+                        return WalkState::Quit;
+                    }
+                },
+                Err(e) => {
+                    *err.lock().unwrap() = Some(e.into());
+                    return WalkState::Quit;
+                }
+            }
+            WalkState::Continue
+        })
+    });
 
-    // Wait for the walker to finish.
-    let (lock, cvar) = &*finish_cv;
-    let mut finished = lock.lock().unwrap();
-    while !*finished {
-        finished = cvar.wait(finished).unwrap();
-    }
     if let Some(e) = err.lock().unwrap().take() {
         return Err(e);
     }
