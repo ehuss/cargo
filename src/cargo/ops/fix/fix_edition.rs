53d78c6b2465c200943f84f45cd6e528b01a9fc0
Eric Huss <eric@huss.org>
2026-06-12 07:18:19 -0700

    Experimental edition scan

--- src/cargo/ops/fix/fix_edition.rs
+++ src/cargo/ops/fix/fix_edition.rs
@@ -15,12 +15,14 @@ pub fn fix_edition(
     opts: &mut FixOptions,
     fix_edition: &FixEdition,
 ) -> CargoResult<()> {
-    let packages = opts.compile_opts.spec.get_packages(original_ws)?;
-    let skip_if_not_edition = |edition| -> CargoResult<bool> {
-        if !packages.iter().all(|p| p.manifest().edition() == edition) {
+    let all_at_latest = |packages: &[&Package]| -> CargoResult<bool> {
+        if packages
+            .iter()
+            .all(|p| p.manifest().edition() == Edition::Edition2024)
+        {
             gctx.shell().status(
-                "Skipping",
-                &format!("not all packages are at edition {edition}"),
+                "crater-edition-check",
+                &format!("all packages now at latest edition"),
             )?;
             Ok(true)
         } else {
@@ -29,79 +31,93 @@ pub fn fix_edition(
     };
 
     match fix_edition {
-        FixEdition::Start(edition) => {
+        FixEdition::Start(_edition) => {
             // The start point just runs `cargo check` if the edition is the
             // starting edition. This is so that crater can set a baseline of
             // whether or not the package builds at all. For other editions,
             // we skip entirely since they are not of interest since we can't
             // migrate them.
-            if skip_if_not_edition(*edition)? {
-                return Ok(());
-            }
+            // if skip_if_not_edition(*edition)? {
+            //     return Ok(());
+            // }
             ops::compile(&original_ws, &opts.compile_opts)?;
         }
-        FixEdition::End { initial, next } => {
+        FixEdition::End {
+            initial: _,
+            next: _,
+        } => {
             // Skip packages that are not the starting edition, since we can
             // only migrate from one edition to the next.
-            if skip_if_not_edition(*initial)? {
-                return Ok(());
-            }
-            // Do the edition fix.
-            opts.edition = Some(EditionFixMode::OverrideSpecific(*next));
-            opts.allow_dirty = true;
-            opts.allow_no_vcs = true;
-            opts.allow_staged = true;
-            ops::fix(gctx, original_ws, opts)?;
-            // Do `cargo check` with the new edition so that we can verify
-            // that it also works on the next edition.
-            replace_edition(&packages, *next)?;
-            gctx.shell()
-                .status("Updated", &format!("edition to {next}"))?;
-            let ws = original_ws.reload(gctx)?;
-            // Unset these since we just want to do a normal `cargo check`.
-            *opts
-                .compile_opts
-                .build_config
-                .rustfix_diagnostic_server
-                .borrow_mut() = None;
-            opts.compile_opts.build_config.primary_unit_rustc = None;
+            // if skip_if_not_edition(*initial)? {
+            //     return Ok(());
+            // }
+            let mut ws = original_ws.reload(gctx)?;
+            while !all_at_latest(&opts.compile_opts.spec.get_packages(&ws)?)? {
+                eprintln!("crater-edition-check: migrating to next edition");
 
-            ops::compile(&ws, &opts.compile_opts)?;
+                // Do the edition fix.
+                opts.edition = Some(EditionFixMode::NextRelative);
+                opts.allow_dirty = true;
+                opts.allow_no_vcs = true;
+                opts.allow_staged = true;
+                ops::fix(gctx, original_ws, opts)?;
+                // Do `cargo check` with the new edition so that we can verify
+                // that it also works on the next edition.
+                let packages = opts.compile_opts.spec.get_packages(&ws)?;
+                replace_edition(&packages)?;
+                ws = original_ws.reload(gctx)?;
+                // Unset these since we just want to do a normal `cargo check`.
+                *opts
+                    .compile_opts
+                    .build_config
+                    .rustfix_diagnostic_server
+                    .borrow_mut() = None;
+                opts.compile_opts.build_config.primary_unit_rustc = None;
+
+                ops::compile(&ws, &opts.compile_opts)?;
+            }
         }
     }
+    eprintln!("crater-edition-check: all finished");
     Ok(())
 }
 
 /// Modifies the `edition` value of the given packages to the new edition.
-fn replace_edition(packages: &[&Package], to_edition: Edition) -> CargoResult<()> {
+fn replace_edition(packages: &[&Package]) -> CargoResult<()> {
     for package in packages {
         let mut manifest_mut = LocalManifest::try_new(package.manifest_path())?;
         let document = &mut manifest_mut.data;
         let root = document.as_table_mut();
         // Update edition to the new value.
+        let current_edition = package.manifest().edition();
+        let next_edition = current_edition.saturating_next();
+        eprintln!(
+            "crater-edition-check: {package} updating edition from {current_edition} to {next_edition}"
+        );
+
         if let Some(package) = root.get_mut("package").and_then(|t| t.as_table_like_mut()) {
             package.insert(
                 "edition",
-                Item::Value(Value::String(Formatted::new(to_edition.to_string()))),
+                Item::Value(Value::String(Formatted::new(next_edition.to_string()))),
             );
         }
         // If the edition is unstable, add it to cargo-features.
-        if !to_edition.is_stable() {
-            let feature = "unstable-editions";
+        // if !to_edition.is_stable() {
+        //     let feature = "unstable-editions";
 
-            if let Some(features) = root
-                .entry("cargo-features")
-                .or_insert_with(|| Item::Value(Value::Array(toml_edit::Array::new())))
-                .as_array_mut()
-            {
-                if !features
-                    .iter()
-                    .any(|f| f.as_str().map_or(false, |f| f == feature))
-                {
-                    features.push(feature);
-                }
-            }
-        }
+        //     if let Some(features) = root
+        //         .entry("cargo-features")
+        //         .or_insert_with(|| Item::Value(Value::Array(toml_edit::Array::new())))
+        //         .as_array_mut()
+        //     {
+        //         if !features
+        //             .iter()
+        //             .any(|f| f.as_str().map_or(false, |f| f == feature))
+        //         {
+        //             features.push(feature);
+        //         }
+        //     }
+        // }
         manifest_mut.write()?;
     }
     Ok(())
